536e811fdbfcfc125cd0cc64799d14449e89c455
Chris Perez <chris.k.perez@gmail.com>
2016-11-03 10:09:17 -0700

    working in c#

---
+++ fun-challenges/cs/Fizzbuzz.cs
@@ -0,0 +1,28 @@
+using System;
+
+  class FizzBuzz
+  {
+        static void Main()
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+
